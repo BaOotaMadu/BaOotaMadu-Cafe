77d44f07178dc17113b7737f1ed896b72450f240
Hiral Jain <hiralkjain21@gmail.com>
2025-03-21 12:10:21 +0530

    Updated Readme

--- BaOotaMadu-backend/index.js
+++ BaOotaMadu-backend/index.js
@@ -9,10 +9,10 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.send('Food App');
 })
-app.use('/auth',require('./routes/authroutes'));
-app.use('/user',require('./routes/userRoutes'));
-app.use('/orders',require('./routes/orderRoutes'));
-app.use('/tableInfo',require('./routes/tableRoutes'));
+app.use('/auth',require('./routes/authroutes.js'));
+app.use('/user',require('./routes/userRoutes.js'));
+app.use('/orders',require('./routes/orderRoutes.js'));
+app.use('/tableInfo',require('./routes/tableRoutes.js'));
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
